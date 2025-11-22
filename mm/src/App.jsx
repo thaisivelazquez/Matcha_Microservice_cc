34ea82fdf932fdfdb23f825d48e00ca105483efe
thaisivelazquez <thaisivelazquez@gmail.com>
2025-11-22 18:25:10 -0500

    added route to the search page

--- mm/src/App.jsx
+++ mm/src/App.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import LoginPage from './pages/Login_page/login_page';
 import Home from './pages/Home/Home';
 import Page1 from './pages/page1/page1';
 import Page2 from './pages/page2/page2';
-import LoginPage from './pages/Login_page/login_page';
 import PrefPage from './pages/pref_page/pref_page';
+import SearchPage from './pages/search/SearchPage'; 
+
 
 const App = () => {
   return (
     <Router>
       <div className='app'>
         <Routes>
-          <Route path='/Login_page' element={<LoginPage />} />
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/home' element={<Home />} />
           <Route path='/Setpreferances' element={<PrefPage />} />  
           <Route path='/page1' element={<Page1 />} />
           <Route path='/page2' element={<Page2 />} />
+          <Route path='/search' element={<SearchPage />} /> 
         </Routes>
       </div>
     </Router>
